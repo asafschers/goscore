21733db93318a415d6cfaddc3eff44c963bee592
asaf schers <schers@riskified.com>
2017-08-19 14:07:58 +0300

    TestSimplePredicate

---
+++ simpe_predicate_test.go
@@ -0,0 +1,50 @@
+package goscore_test
+
+import (
+	"testing"
+	"encoding/xml"
+	"github.com/asafschers/goscore"
+)
+
+var simplePredicateTests = []struct {
+	predicate	[]byte
+	features    map[string]string
+	expected	bool
+}{
+	{[]byte(`<SimplePredicate field="f33" operator="equal" value="18.85"/>`),
+		map[string]string{"f33": "18.850"},
+		true},
+	{[]byte(`<SimplePredicate field="f33" operator="lessOrEqual" value="18.85"/>`),
+	map[string]string{"f33": "18.84"},
+	true},
+	{[]byte(`<SimplePredicate field="f33" operator="lessOrEqual" value="18.85"/>`),
+		map[string]string{"f33": "18.86"},
+		false},
+	{[]byte(`<SimplePredicate field="f33" operator="isMissing" value="18.85"/>`),
+		map[string]string{"f33": "18.86"},
+		false},
+	{[]byte(`<SimplePredicate field="f33" operator="isMissing" value="18.85"/>`),
+		map[string]string{},
+		true},
+}
+
+func TestSimplePredicate(t *testing.T) {
+
+	for _, tt := range simplePredicateTests {
+		var predicate goscore.SimplePredicate
+		xml.Unmarshal(tt.predicate, &predicate)
+
+		actual := predicate.True(tt.features)
+		if actual != tt.expected {
+			t.Errorf("Predicate: %s %s %s, Feature value : %s, expected %t, actual %t",
+				predicate.Field,
+				predicate.Operator,
+				predicate.Value,
+				tt.features[predicate.Field],
+				tt.expected,
+				actual)
+		}
+	}
+}
+
+
