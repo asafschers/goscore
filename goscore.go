15cad485f6d0a984d0a35f97f4d8d07cdb495e9e
asaf schers <schers@riskified.com>
2017-09-19 22:48:55 +0300

    PMMLs to fixtures dir

--- goscore.go
+++ goscore.go
@@ -1,4 +1,4 @@
 /*
 Package goscore is a go scoring API for PMML files
- */
+*/
 package goscore
--- tree_test.go
+++ tree_test.go
@@ -4,60 +4,8 @@ import (
 	"encoding/xml"
 	"github.com/asafschers/goscore"
 	"testing"
+	"io/ioutil"
 )
-
-const treeXml = `
-<TreeModel functionName="regression" splitCharacteristic="multiSplit">
-    <Node id="1">
-        <True/>
-        <Node id="13" score="4.3463944950723456E-4">
-            <SimplePredicate field="f2" operator="isMissing"/>
-        </Node>
-        <Node id="2">
-            <SimpleSetPredicate field="f2" booleanOperator="isIn">
-                <Array type="string">f2v1 f2v2 f2v3</Array>
-            </SimpleSetPredicate>
-            <Node id="11" score="-1.8361380219689046E-4">
-                <SimplePredicate field="f1" operator="isMissing"/>
-            </Node>
-            <Node id="3">
-                <SimpleSetPredicate field="f1" booleanOperator="isIn">
-                    <Array type="string">f1v1 f1v2 f1v3</Array>
-                </SimpleSetPredicate>
-                <Node id="9" score="-6.237581139073701E-4">
-                    <SimplePredicate field="f4" operator="isMissing"/>
-                </Node>
-                <Node id="4" score="0.0021968294712358194">
-                    <SimplePredicate field="f4" operator="lessThan" value="0.08086312118570185"/>
-                </Node>
-                <Node id="5">
-                    <SimplePredicate field="f4" operator="greaterOrEqual" value="0.08086312118570185"/>
-                    <Node id="8" score="-9.198573460887271E-4">
-                        <SimplePredicate field="f3" operator="isMissing"/>
-                    </Node>
-                    <Node id="6" score="-0.0021187239505556523">
-                        <SimpleSetPredicate field="f3" booleanOperator="isIn">
-                            <Array type="string">f3v1 f3v2 f3v3</Array>
-                        </SimpleSetPredicate>
-                    </Node>
-                    <Node id="7" score="-3.3516227414227926E-4">
-                        <SimpleSetPredicate field="f3" booleanOperator="isIn">
-                            <Array type="string">f3v4 f3v5 f3v6</Array>
-                        </SimpleSetPredicate>
-                    </Node>
-                </Node>
-            </Node>
-            <Node id="10" score="0.0011015286521365208">
-                <SimpleSetPredicate field="f1" booleanOperator="isIn">
-                    <Array type="string">f1v4 f1v5 f1v6</Array>
-                </SimpleSetPredicate>
-            </Node>
-        </Node>
-        <Node id="12" score="0.0022726641744997256">
-            <SimplePredicate field="f2" operator="equal" value="f2v4"/>
-        </Node>
-    </Node>
-</TreeModel>`
 
 var TreeTests = []struct {
 	features map[string]string
@@ -105,6 +53,11 @@ var TreeTests = []struct {
 // TODO: errors on unknown value
 
 func TestTree(t *testing.T) {
+	treeXml, err := ioutil.ReadFile("fixtures/tree.pmml")
+	if err != nil {
+		panic(err)
+	}
+
 	tree := []byte(treeXml)
 	var n goscore.Node
 	xml.Unmarshal(tree, &n)
