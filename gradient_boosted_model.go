a553ba088f483d6e6a344ef0afa43d342a4a7979
asaf schers <schers@riskified.com>
2017-09-22 22:59:01 +0300

    TestGradientBoostedModel  and GradientBoostedModel

---
+++ gradient_boosted_model.go
@@ -0,0 +1,32 @@
+package goscore
+
+import (
+	"encoding/xml"
+	"math"
+)
+
+// GradientBoostedModel - GradientBoostedModel PMML
+type GradientBoostedModel struct {
+	XMLName xml.Name
+	Trees   []Node `xml:"MiningModel>Segmentation>Segment>MiningModel>Segmentation>Segment>TreeModel"`
+	Target  target `xml:"MiningModel>Segmentation>Segment>MiningModel>Targets>Target"`
+}
+
+type target struct {
+	XMLName         xml.Name
+	RescaleConstant float64 `xml:"rescaleConstant,attr"`
+}
+
+// Score - traverses all trees in GradientBoostedModel with features and returns exp(sum) / (1 + exp(sum))
+// where sum is sum of trees + rescale constant
+func (gbm GradientBoostedModel) Score(features map[string]string) (float64, error) {
+	sum := gbm.Target.RescaleConstant
+	for _, tree := range gbm.Trees {
+		score, err := tree.TraverseTree(features)
+		if err != nil {
+			return -1, err
+		}
+		sum += score
+	}
+	return math.Exp(sum) / (1 + math.Exp(sum)), nil
+}
---
+++ gradient_boosted_model_test.go
@@ -0,0 +1,68 @@
+package goscore_test
+
+import (
+	"encoding/xml"
+	"github.com/asafschers/goscore"
+	"io/ioutil"
+	"math"
+	"testing"
+)
+
+var GradientBoostedModelTests = []struct {
+	features map[string]string
+	score    float64
+}{
+	{map[string]string{
+		"Sex":      "male",
+		"Parch":    "0",
+		"Age":      "30",
+		"Fare":     "9.6875",
+		"Pclass":   "2",
+		"SibSp":    "0",
+		"Embarked": "Q",
+	},
+		0.3652639329522468,
+	},
+	{map[string]string{
+		"Sex":      "female",
+		"Parch":    "0",
+		"Age":      "38",
+		"Fare":     "71.2833",
+		"Pclass":   "2",
+		"SibSp":    "1",
+		"Embarked": "C",
+	},
+		0.4178155014037758,
+	},
+}
+
+const TOLERANCE = 0.000001
+
+func TestGradientBoostedModel(t *testing.T) {
+	GradientBoostedModelXml, err := ioutil.ReadFile("fixtures/gradient_boosted_model.pmml")
+	if err != nil {
+		panic(err)
+	}
+
+	var gbm goscore.GradientBoostedModel
+	GradientBoostedModel := []byte(GradientBoostedModelXml)
+	err = xml.Unmarshal(GradientBoostedModel, &gbm)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(gbm.Trees) != 100 {
+		t.Errorf("expected 100 trees, actual %d", len(gbm.Trees))
+	}
+
+	for _, tt := range GradientBoostedModelTests {
+
+		actual, _ := gbm.Score(tt.features)
+
+		if diff := math.Abs(actual - tt.score); diff > TOLERANCE {
+			t.Errorf("expected %f, actual %f",
+				tt.score,
+				actual)
+		}
+	}
+}
