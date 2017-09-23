31af9194a4e15629162831c9726aa53ca8de101b
asaf schers <schers@riskified.com>
2017-09-23 23:54:43 +0300

    GradientBoostedModel - fetch const by PMML version

--- gradient_boosted_model.go
+++ gradient_boosted_model.go
@@ -7,9 +7,10 @@ import (
 
 // GradientBoostedModel - GradientBoostedModel PMML
 type GradientBoostedModel struct {
-	XMLName xml.Name
-	Trees   []Node `xml:"MiningModel>Segmentation>Segment>MiningModel>Segmentation>Segment>TreeModel"`
-	Target  target `xml:"MiningModel>Segmentation>Segment>MiningModel>Targets>Target"`
+	Version  string  `xml:"version,attr"`
+	Trees    []Node  `xml:"MiningModel>Segmentation>Segment>MiningModel>Segmentation>Segment>TreeModel"`
+	Target   target  `xml:"MiningModel>Segmentation>Segment>MiningModel>Targets>Target"`
+	Constant float64 `xml:"MiningModel>Segmentation>Segment>MiningModel>Output>OutputField>Apply>Constant"`
 }
 
 type target struct {
@@ -20,7 +21,9 @@ type target struct {
 // Score - traverses all trees in GradientBoostedModel with features and returns exp(sum) / (1 + exp(sum))
 // where sum is sum of trees + rescale constant
 func (gbm GradientBoostedModel) Score(features map[string]interface{}) (float64, error) {
-	sum := gbm.Target.RescaleConstant
+
+	sum := fetchConst(gbm)
+
 	for _, tree := range gbm.Trees {
 		score, err := tree.TraverseTree(features)
 		if err != nil {
@@ -29,4 +32,12 @@ func (gbm GradientBoostedModel) Score(features map[string]interface{}) (float64,
 		sum += score
 	}
 	return math.Exp(sum) / (1 + math.Exp(sum)), nil
+}
+func fetchConst(gbm GradientBoostedModel) (sum float64) {
+	if gbm.Version == "4.2" {
+		sum = gbm.Constant
+	} else if gbm.Version == "4.3" {
+		sum = gbm.Target.RescaleConstant
+	}
+	return sum
 }
