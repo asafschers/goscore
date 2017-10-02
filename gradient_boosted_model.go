4109c2bc554562b5895b4ebcb826ad6c5eb89db0
asaf schers <schers@riskified.com>
2017-10-02 17:52:51 +0300

    GradientBoostedModel - extract method sumScores

--- gradient_boosted_model.go
+++ gradient_boosted_model.go
@@ -41,31 +41,38 @@ type result struct {
 
 // ScoreConcurrently - same as Score but concurrent
 func (gbm GradientBoostedModel) ScoreConcurrently(features map[string]interface{}) (float64, error) {
-	sum := fetchConst(gbm)
 	messages := make(chan result, len(gbm.Trees))
-
 	var wg sync.WaitGroup
 	wg.Add(len(gbm.Trees))
 
 	for _, tree := range gbm.Trees {
 		go func(tree Node, features map[string]interface{}) {
 			treeScore, err := tree.TraverseTree(features)
-
 			messages <- result{ErrorName: err, Score: treeScore}
 			wg.Done()
 		}(tree, features)
 	}
 	wg.Wait()
 
-	var res result
-	for i := 0; i < len(gbm.Trees); i++ {
-		res = <-messages
+	sum, err := sumScores(messages, len(gbm.Trees))
+	if err != nil {
+		return -1, err
+	}
+	sum += fetchConst(gbm)
+
+	return math.Exp(sum) / (1 + math.Exp(sum)), nil
+}
+
+func sumScores(messages chan result, treeCount int) (float64, error){
+	sum := 0.0
+	for i := 0; i < treeCount; i++ {
+		res := <-messages
 		if res.ErrorName != nil {
 			return -1, res.ErrorName
 		}
 		sum += res.Score
 	}
-	return math.Exp(sum) / (1 + math.Exp(sum)), nil
+	return sum, nil
 }
 
 func fetchConst(gbm GradientBoostedModel) (sum float64) {
