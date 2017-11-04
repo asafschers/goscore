e8757f3bfac80057c3c684ee53f97b0f85547364
asaf schers <schers@riskified.com>
2017-11-04 13:17:57 +0200

    GradientBoostedModelTest - ginkgo

--- gradient_boosted_model_test.go
+++ gradient_boosted_model_test.go
@@ -3,74 +3,78 @@ package goscore_test
 import (
 	"encoding/xml"
 	"github.com/asafschers/goscore"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"io/ioutil"
-	"math"
 	"testing"
 )
 
-var GradientBoostedModelTests = []struct {
-	features map[string]interface{}
-	score    float64
-}{
-	{map[string]interface{}{
-		"Sex":      "male",
-		"Parch":    0,
-		"Age":      30,
-		"Fare":     9.6875,
-		"Pclass":   2,
-		"SibSp":    0,
-		"Embarked": "Q",
-	},
-		0.3652639329522468,
-	},
-	{map[string]interface{}{
-		"Sex":      "female",
-		"Parch":    0,
-		"Age":      38,
-		"Fare":     71.2833,
-		"Pclass":   2,
-		"SibSp":    1,
-		"Embarked": "C",
-	},
-		0.4178155014037758,
-	},
-}
+var _ = Describe("GradientBoostedModel", func() {
+	var (
+		lowScoreFeatures, highScoreFeatures map[string]interface{}
+		highScore, lowScore                 float64
+		tolerance                           float64 = 0.000000001
+		gbm                                 goscore.GradientBoostedModel
+		first                               bool    = true
+	)
 
-const TOLERANCE = 0.000001
+	BeforeSuite(func() {
+		lowScoreFeatures = map[string]interface{}{
+			"Sex":      "male",
+			"Parch":    0,
+			"Age":      30,
+			"Fare":     9.6875,
+			"Pclass":   2,
+			"SibSp":    0,
+			"Embarked": "Q",
+		}
+		lowScore = 0.3652639329522468
+
+		highScoreFeatures = map[string]interface{}{
+			"Sex":      "female",
+			"Parch":    0,
+			"Age":      38,
+			"Fare":     71.2833,
+			"Pclass":   2,
+			"SibSp":    1,
+			"Embarked": "C",
+		}
+		highScore = 0.4178155014037758
+
+		if first {
+			GradientBoostedModelXml, err := ioutil.ReadFile("fixtures/gradient_boosted_model.pmml")
+			if err != nil {
+				panic(err)
+			}
+
+			err = xml.Unmarshal([]byte(GradientBoostedModelXml), &gbm)
+			if err != nil {
+				panic(err)
+			}
+			first = false
+		}
+	})
+
+	Describe("Scores", func() {
+		It("Scores low", func() {
+			Expect(gbm.Score(lowScoreFeatures)).To(BeNumerically("~", lowScore, tolerance))
+		})
+
+		It("Scores low concurrently", func() {
+			Expect(gbm.ScoreConcurrently(lowScoreFeatures)).To(BeNumerically("~", lowScore, tolerance))
+		})
+
+		It("Scores high", func() {
+			Expect(gbm.Score(highScoreFeatures)).To(BeNumerically("~", highScore, tolerance))
+		})
+
+		It("Scores high concurrently", func() {
+			Expect(gbm.ScoreConcurrently(highScoreFeatures)).To(BeNumerically("~", highScore, tolerance))
+		})
+	})
+})
 
 func TestGradientBoostedModel(t *testing.T) {
-	GradientBoostedModelXml, err := ioutil.ReadFile("fixtures/gradient_boosted_model.pmml")
-	if err != nil {
-		panic(err)
-	}
-
-	var gbm goscore.GradientBoostedModel
-	err = xml.Unmarshal([]byte(GradientBoostedModelXml), &gbm)
-	if err != nil {
-		panic(err)
-	}
-
-	if len(gbm.Trees) != 100 {
-		t.Errorf("expected 100 trees, actual %d", len(gbm.Trees))
-	}
-
-	for _, tt := range GradientBoostedModelTests {
-
-		actual, _ := gbm.Score(tt.features)
-
-		if diff := math.Abs(actual - tt.score); diff > TOLERANCE {
-			t.Errorf("expected %f, actual %f",
-				tt.score,
-				actual)
-		}
-
-		actual, _ = gbm.ScoreConcurrently(tt.features)
-
-		if diff := math.Abs(actual - tt.score); diff > TOLERANCE {
-			t.Errorf("expected %f, actual %f",
-				tt.score,
-				actual)
-		}
-
-	}
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "GradientBoostedModel Suite")
 }
